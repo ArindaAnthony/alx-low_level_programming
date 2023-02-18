04547ce0f693a4ae924a09aa38368f5726652a6d
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-18 01:47:46 -0800

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19");
+	return (0);
+}
