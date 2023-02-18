cd063a2c8e59b58cd732e42540855438843a9412
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-18 05:58:27 -0800

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - this is the main function
  *
- * Return: always 1
+ * Return: 1(Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
