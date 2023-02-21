dd16b9323eefe295c21e07584d45a6c3a2d666f8
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-21 08:44:11 -0800

    hei

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - this is the main function
+ * Return:always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
