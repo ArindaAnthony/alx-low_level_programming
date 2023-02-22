76afa36f24eeb03daeb42867b3b34ec787950e0c
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-22 07:03:08 -0800

    God did

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - is the main function
+ * @m: parameter
+ * @n: parameter 
+ * Return: sum of two numbers
+ */
+int add(int m, int n)
+{
+	int c;
+
+	c = m + n;
+	return (c);
+}
