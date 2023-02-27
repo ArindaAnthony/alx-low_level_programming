01c779d5f309ea77c02e1d61de5bff559fe4fb98
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-27 02:16:35 -0800

    it worked

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - a function that prints a string, followed by a new line
+ * @str: parameter
+ * Return: always void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
