1e760c441ee65a256d486f54e96d2560dde81d7d
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-26 23:47:36 -0800

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - function to update the number
+ * @n: parameter
+ * Return: always void
+ */
+void reset_to_98(int *n)
+{
+	int *p = n;
+
+	*p = 98;
+}
