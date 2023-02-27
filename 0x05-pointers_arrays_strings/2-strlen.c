a9385809f02127b7abbf4c5327963fc4a3b7a73b
ArindaAnthony <arindaanthony69@gmail.com>
2023-02-27 00:59:06 -0800

    Using the strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - returns length of a string
+ * @s: parameter
+ * Return: always void
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+	return (n);
+}
