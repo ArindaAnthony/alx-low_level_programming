72df7285e890fb8f335ffcbcca75adce669391e1
ArindaAnthony <arindaanthony69@gmail.com>
2023-03-01 08:55:16 -0800

    hei

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - this is the main function
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameteur
+ * Return: always string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
