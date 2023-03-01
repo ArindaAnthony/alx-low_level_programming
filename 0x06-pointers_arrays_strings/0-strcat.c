b55c6be759444f1dc86672998a7525e5834821d9
ArindaAnthony <arindaanthony69@gmail.com>
2023-03-01 04:54:41 -0800

    header file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcat - this is afunction that one string to the other
+ * @dest: parameter
+ * @src: parameter
+ * Return: always char
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}
