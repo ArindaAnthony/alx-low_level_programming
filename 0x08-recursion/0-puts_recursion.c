dc3c286511351b56fca6b23ed6cbd50310612684
ArindaAnthony <arindaanthony69@gmail.com>
2023-03-08 10:14:07 -0800

    recurcsion 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - function
+ * @s: parameter
+ * Return: always void
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s", s);
+	printf("\n");
+}
